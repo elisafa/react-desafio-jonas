33d033f220c822984e70b514e8de1ac14ba81ae3
Elisafa <elisafajunior@hotmail.com>
2023-12-21 14:03:56 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Count />
     </div>
-  );
+  )
 }
 
-export default App;
+// function Steps() {
+//   const [steps, setSteps] = useState(0)
+
+//   return (
+//     <div className="steps">
+//       <button onClick={() => setSteps((s) => s - 5)}>-</button>
+//       <div>Step: {steps}</div>
+//       <button onClick={() => setSteps((s) => s + 5)}>+</button>
+//     </div>
+//   )
+// }
+
+function Count() {
+  const [count, setCount] = useState(0)
+  const [step, setStep] = useState(1)
+
+  const data = new Date()
+  data.setDate(data.getDate() + count)
+  return (
+    <>
+      <div className="contador">
+        <button onClick={() => setStep((c) => c - 1)}>-</button>
+        <div>Step:{step} </div>
+        <button onClick={() => setStep((c) => c + 1)}>+</button>
+      </div>
+      <div className="contador">
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <div>Count:{count} </div>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count >= 0
+            ? `${count} day from today is `
+            : `${count} days ago was `}
+        </span>
+        <span>{data.toDateString()}</span>
+      </p>
+    </>
+  )
+}
